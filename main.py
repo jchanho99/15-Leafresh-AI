7264132896fa4fbb91e157c19cbbd7079b3fb72d
kellyn0522 <103016025+kellyn0522@users.noreply.github.com>
2025-05-07 15:58:11 +0900

    change router

--- main.py
+++ main.py
@@ -5,15 +5,13 @@ from fastapi import FastAPI
 import threading
 
 from worker import run_worker
-from router import router
+from verify_router import router as verify_router
 
-# 환경 변수 불러오기
 load_dotenv()
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.getenv("GOOGLE_APPLICATION_CREDENTIALS")
 
 # app 초기화
 app = FastAPI()
-app.include_router(router)
+app.include_router(verify_router)
 
 # worker를 main 실행할 때 지속적으로 실행되도록 변경 
 # pubsub_v1이 동기로 실행되므로 async를 붙이지 않음 
